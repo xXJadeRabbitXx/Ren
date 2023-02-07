b26957d62f74182d83e63a6f6384a9abbefa69e7
Lui <injabie3@gmail.com>
2023-02-06 22:11:00 -0800

    [QRChecker] Add max pixel configuration (#618)
    * [QRChecker] Add max pixel configuration
    
    - On systems with low RAM, loading the image into RAM via Pillow will
      cause the bot to use too much RAM, invoke OOM kille and get killed.
      This is undesirable.
    - Only allow one image to be loaded into RAM at one time by using a
      lock. This will reduce the chance of running out of memory.
    
    * [QRChecker] Add `show` command
    
    * [QRChecker] Rename `KEY_MAX_IMAGE_PIXELS` constant
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>
    
    * [QRChecker] Fix dumb `black` formatting
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>
    
    * [QRChecker] Refactor max size initialization
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>
    
    * [QRChecker] Fix strings in `show` command
    
    * [QRChecker] Rename `size` command to `maxpixels`
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>
    
    * [QRChecker] Divide max pixels by 2 when setting
    
    - By default, `DecompressionBombError` occurs when the image is twice
      the MAX_IMAGE_PIXELS value. As such, divide the value by 2 so that
      the actual max pixels value will cause `DecompressionBombError`.
    
    * [QRChecker] Re-add docstring to `toggle` command
    
    - Required to show in command help.
    
    * [QRChecker] Run through `black`
    
    * [QRChecker] Apply suggestions from code review
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>
    
    * [QRChecker] Add divide by 2 rationale for MAX_IMAGE_PIXELS
    
    ---------
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>

--- cogs/qrchecker/commandHandlers.py
+++ cogs/qrchecker/commandHandlers.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from redbot.core import commands
 from redbot.core.commands import Context
 
@@ -13,4 +15,25 @@ class CommandHandlers(CommandsCore):
 
     @_grpQrChecker.command(name="toggle")
     async def _cmdQrCheckerToggle(self, ctx: Context):
+        """Toggle QR code checking"""
         await self.cmdQrCheckerToggle(ctx=ctx)
+
+    @_grpQrChecker.command("show")
+    async def _cmdQrCheckerShow(self, ctx: Context):
+        """Show current settings"""
+        await self.cmdQrCheckerShow(ctx=ctx)
+
+    @_grpQrChecker.command(name="maxpixels")
+    async def _cmdQrCheckerMaxPixels(self, ctx: Context, *, pixels: Optional[int]):
+        """Set the maximum image pixels to check.
+
+        Binary images are loaded into RAM in Pillow, which uses RAM. If your bot is
+        running on a system with limited RAM, set this to a low value to avoid OOM
+        killer from killing your bot.
+
+        Parameters
+        ----------
+        pixels: Optional[int]
+            The maximum number of pixels in an image to check.
+        """
+        await self.cmdQrCheckerMaxPixels(ctx=ctx, pixels=pixels)
