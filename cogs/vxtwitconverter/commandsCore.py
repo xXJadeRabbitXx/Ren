59f2ab1591521be24087e781adf4e1af28af6583
Lui <injabie3@gmail.com>
2023-06-17 11:47:10 -0700

    [VxTwit] Add per-guild toggle (#649)
    * [VxTwit] Add per-guild toggle
    
    This commit adds the ability to toggle the VxTwitter replacements on a
    per-guild basis. It requires the use of Config to store the enabled
    state, which is then used within the event handler.
    
    By default, it is disabled. It can be toggled by the guild admin using the
    `[p]vxtwit toggle` command.
    
    * [VxTwit] Extract enable to variable
    
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>

---
+++ cogs/vxtwitconverter/commandsCore.py
@@ -0,0 +1,14 @@
+from redbot.core.commands.context import Context
+
+from .core import Core
+from .constants import KEY_ENABLED
+
+
+class CommandsCore(Core):
+    async def cmdToggle(self, ctx: Context):
+        enabledCfg = self.config.guild(ctx.guild).get_attr(KEY_ENABLED)
+        enabled = not await enabledCfg()
+        await enabledCfg.set(enabled)
+
+        status = "enabled" if enabled else "disabled"
+        await ctx.send(f"VxTwitter replacements are now {status}.")
